8b9a31222e9ec0e59539d323c29b2707beff02d4
masonictemple4 <masonictemple4@users.noreply.github.com>
2026-02-05 07:47:39 +0000

    chore: export KanbanBoardConnected from board index

--- app/components/board/index.ts
+++ app/components/board/index.ts
@@ -1,4 +1,5 @@
 export { KanbanBoard, generateMockData } from "./KanbanBoard";
+export { KanbanBoardConnected } from "./KanbanBoardConnected";
 export { KanbanColumn, SimpleKanbanColumn } from "./KanbanColumn";
 export { TaskCard, DraggableTaskCard } from "./TaskCard";
 export type { Task, Column, BoardState, DragData, DragType } from "./types";
